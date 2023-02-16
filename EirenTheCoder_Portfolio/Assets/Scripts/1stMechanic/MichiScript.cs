81746d0782a64edc4b3f350a10e01a4874e6ebcc
Aaron del Risco <jdelriscodiaz@gmail.com>
2023-02-15 20:18:30 -0500

    avance Michi

--- EirenTheCoder_Portfolio/Assets/Scripts/1stMechanic/MichiScript.cs
+++ EirenTheCoder_Portfolio/Assets/Scripts/1stMechanic/MichiScript.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class MichiScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    enum TypeOfBuilding {
+    
+    tally,shorty,temporal
+    
+    };
+    private int a;
+    private TypeOfBuilding building;
+    [SerializeField] private float speed;
     // Update is called once per frame
     void Update()
     {
         
     }
+
+    void reasoning() {
+
+        switch (building) {
+
+            case TypeOfBuilding.tally:
+                
+
+                
+                
+                break;
+            case TypeOfBuilding.shorty:
+                
+                
+                break;
+
+            case TypeOfBuilding.temporal:
+
+
+                break;
+
+        };
+    }
+    void spawn() {
+        
+        a = Random.Range(0, 3);
+
+        switch (a) {
+
+            case 0:
+                
+                break;
+            case 1:break;
+            case 2:break;
+        
+        }
+    
+    
+    }
+    
+
 }
