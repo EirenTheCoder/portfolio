8676e5fdf5c2d052b5a28ee96d9fe70b22f52cc3
ATDA05 <jdelriscodiaz@gmail.com>
2023-05-31 09:06:05 -0500

    Update WaterScript.cs

--- EirenTheCoder_Portfolio/Assets/Scripts/ThirdMechanic/WaterScript.cs
+++ EirenTheCoder_Portfolio/Assets/Scripts/ThirdMechanic/WaterScript.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class WaterScript : MonoBehaviour
 {
+    private Agent agent;
     // Start is called before the first frame update
     void Start()
     {
-        
+     agent = GetComponent<Agent>();   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.addFlee(new FleeParameters{
+
+            target = Input.mousePosition,
+            range = 5f;
+            weight= 1f;
+
+        });
+        agent.Update();
     }
 }
